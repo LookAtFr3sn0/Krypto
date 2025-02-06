6c50fb7291f28d61110a3d3760f4cc7b2a007469
LookAtFr3sn0 <lookatfr3sn0@gmail.com>
2025-02-06 23:02:22 +0100

    Add password generation and display entropy

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,9 +1,38 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import { GeneratePassword } from './composables/GeneratePassword.composable';
 import { PasswordStrength } from './composables/PasswordStrength.composable';
 
+const minLen = 4;
+const maxLen = 512;
 
+const password = ref('');
+const passwordStrength = ref(0);
+const charset = ref("abc");
+const length = ref(64);
+
+const update = () => {
+  password.value = GeneratePassword(length.value, charset.value);
+  passwordStrength.value = Math.floor(PasswordStrength(length.value, charset.value));
+}
+
+update();
 </script>
 
 <template>
+  <div>
+    <div class="flex flex-col" >
+      <span class="text-sm pl-1">Password</span>
+      <span class="border-1 border-gray-300 rounded-md min-w-min max-w-md flex-wrap min-h-6">{{ password }}</span>
+      <button @click="update">Generate</button>
+    </div>
+    <div>
+      <div>
+        <label for="length">Length</label>
+        <input @input="update" type="range" id="length" v-model="length" :min="minLen" :max="maxLen" />
+        <label for="length">{{ length }}</label>
+      </div>
+    </div>
+    <span>Entropy: <span class="text-blue-500">{{ passwordStrength }}</span></span>
+  </div>
 </template>
